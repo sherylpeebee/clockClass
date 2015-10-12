7923f555476aab3253d7fa311eeef6c6a938c0a5
Sheryl Boughton <sherpbo@gmail.com>
2015-10-12 10:37:08 -0700

    bug; minute hand resets to first fixedposition after sixty seconds so clock still always starts at same time

--- index.js
+++ index.js
@@ -4,27 +4,28 @@ $(function(){
 
       var date = new Date();
       var hours = date.getHours() > 12 ? date.getHours() - 12 : date.getHours();
-      var hh, mm, ss;
+      var currentHour, currentMinute, currentSecond;
 
-      (function getTime(){
-        hh = hours.toString().length < 2 ? "0" + hours : hours;
-        mm = date.getMinutes().toString().length < 2 ? "0" + date.getMinutes() : date.getMinutes();
-        ss = date.getSeconds().toString().length < 2 ? "0" + date.getSeconds() : date.getSeconds();
+      (function getInitialHandPlacement(){
+        currentHour = hours.toString().length < 2 ? "0" + hours : hours;
+        currentMinute = date.getMinutes().toString().length < 2 ? "0" + date.getMinutes() : date.getMinutes();
+        currentSecond = date.getSeconds().toString().length < 2 ? "0" + date.getSeconds() : date.getSeconds();
       })();
 
-      setInterval(function () {
-        hh = hours.toString().length < 2 ? "0" + hours : hours;
-        mm = date.getMinutes().toString().length < 2 ? "0" + date.getMinutes() : date.getMinutes();
-        ss = date.getSeconds().toString().length < 2 ? "0" + date.getSeconds() : date.getSeconds();
+      var hourIncr =  currentHour < 3 < currentHour ? (currentHour - 3) * 30 : 0;
+      $("div.hour_hand").css({transform: 'rotate(' + hourIncr + 'deg)' });
+
+      var minIncr = currentMinute < 15 < currentMinute ? (currentMinute - 15) * 6 : 0;
+      $("div.minute_hand").css({ transform: 'rotate(' + minIncr + 'deg)' });
+
+      setInterval(function digitalTime() {
+        date = new Date();
+        hours = date.getHours() > 12 ? date.getHours() - 12 : date.getHours();
+        var hh = hours.toString().length < 2 ? "0" + hours : hours;
+        var mm = date.getMinutes().toString().length < 2 ? "0" + date.getMinutes() : date.getMinutes();
+        var ss = date.getSeconds().toString().length < 2 ? "0" + date.getSeconds() : date.getSeconds();
         $(".digital_time").text(hh + ":" + mm + ":" + ss);
       }, 1000);
-
-
-        var hourIncr =  hh < 3 < hh ? (hh - 3) * 30 : 0;
-        $("div.hour_hand").css({transform: 'rotate(' + hourIncr + 'deg)' });
-
-        var minIncr = mm < 15 < mm ? (mm - 15) * 6 : 0;
-        $("div.minute_hand").css({transform: 'rotate(' + minIncr + 'deg)' });
 
         var secsCount = 0;
         setInterval(function(){
@@ -33,7 +34,6 @@ $(function(){
           } else {
             secsCount = 6;
           }
-          // console.log(secsCount);
           $("div.second_hand").css({transform: 'rotate(' + secsCount + 'deg)' });
         }, 1000);
 
@@ -44,20 +44,9 @@ $(function(){
           } else {
             minCount = 6;
           }
-          // console.log("minCount: " + minCount);
           $("div.minute_hand").css({transform: 'rotate(' + minCount + 'deg)' });
         }, 60000);
 
-        // var hourCount = 0;
-        // setInterval(function(){
-        //   if(hourCount < 360){
-        //     hourCount += 6;
-        //   } else {
-        //     hourCount = 6;
-        //   }
-        //   console.log("minCount: " + hourCount);
-        //   $("div.hour_hand").css({transform: 'rotate(' + hourCount + 'deg)' });
-        // }, 60000);
 
     };
   }
@@ -66,7 +55,3 @@ $(function(){
   clock.run();
 
 });
-
-//start all hands at noon.
-//get hour time from clock func and increment 30deg for every past noon
-//every fifteen minutes, hour increment six degrees (one notch)
